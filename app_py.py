49aeff289dc693b9281e65cb3e981b6f1d7202df
Aayush Rijal <132061069+aayushrijalhere@users.noreply.github.com>
2025-06-24 23:16:18 +0545

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1RsbvLUay-vlwj7kCqaED_xFRbjEw4PQR
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
